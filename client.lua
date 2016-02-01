b2cbbb5878d441dd5fb53c58089df72446abae71
Adam Smith <adsmith@nottheeconomist.com>
2016-02-01 13:58:01 -0800

    Begun to implement receiver lists server&client

--- client.lua
+++ client.lua
@@ -10,6 +10,26 @@ local PORT = 15245
 
 local ok = modem.open(PORT)
 
+-- get remote list
+function getReceivers()
+	modem.send(SERVER, PORT, "getReceivers", modem.address)
+	local data = {event.pull("modem_message", nil, nil, PORT, nil, "sendReceivers")}[7]
+	return serialization.unserialize(data)
+end
+
+-- update remove list
+function updateReceivers(newversion)
+	modem.send(SERVER, PORT, "updateReceivers", modem.address, serialization.serialize(newversion))
+	e = event.pull(5, "modem_message", nil, nil, PORT, nil, "ok")
+	if e != nil
+		return 0
+	else
+		return 1
+	end
+end
+
+local receivers = getReceivers()
+
 while true do
 	_, _, remoteAddress, port, _, payload = event.pull("modem_message")
 	if port == PORT then
@@ -20,6 +40,7 @@ while true do
 		elseif code == keyboard.keys.down then print("DOWN!")
 		elseif code == keyboard.keys.left then print("LEFT!")
 		elseif code == keyboard.keys.right then print("RIGHT!")
+		elseif code == keyboard.keys.enter then print("----ENTER----")
 		end
 	end
 
--- server.lua
+++ server.lua
@@ -7,7 +7,11 @@ local modem = component.modem
 local PORT = 15245
 local DEST = "388a64b3-cd16-4a52-8701-1a3bb031a50a"
 
+local RECEIVERDBPATH = "/etc/receiversdb.lua"
+
 local allowedPlayers = {["Areskale"]=true, ["Drunkensaint"]=true, ["rowenlemmings"]=true, }
+
+local receivers = {}
 
 local ok = modem.open(PORT)
 if not ok then
@@ -15,14 +19,66 @@ if not ok then
 	os.exit(1)
 end
 
+function _loadReceiverList(f)
+	function Receiver(t)
+		table.insert(receivers, t)
+	end
+	dofile(f)
+	return receivers
+end
+
+function _saveReceiverList(f)
+	-- receivers should always be a table like:
+	--   Receiver{
+	--     x = 123,
+	--     y = 456,
+	--     z = 789,
+	--   }
+	io.output(f)
+	io.write("Receiver{\n")
+	for k,v in pairs(receivers) do
+		io.write("  " .. k .. " = ", v, ",\n")
+	end
+	io.write("}\n")
+end
+
+function saveReceivers()
+	_saveReceiverList(RECEIVERDBPATH)
+
+function loadReceivers()
+	return _loadReceiverlist(RECEIVERDBPATH)
+end
+
+function updateReceivers(newReceivers)
+	oldReceivers = _loadReceiverList(RECEIVERDBPATH)
+	oldSet = {}
+	new = {}
+	for _, t in ipairs(oldReceivers) do
+		oldSet["x " .. t.x .. ", " ..
+		       "y " .. t.y .. ", " ..
+		       "z " .. t.z] = true
+    end
+    for _, t in ipairs(newReceivers) do
+    	if oldSet["x " .. t.x .. ", " ..
+		          "y " .. t.y .. ", " ..
+		          "z " .. t.z] == nil then
+			table.insert(new, t)
+		end
+	end
+	for _, t in ipairs(new) do
+		table.insert(receivers, t)
+	end
+	saveReceivers()
+end
+
 function handleKeyPress(name, ...)
 	address, char, code, playerName = ...
-	
+
 	if allowedPlayers[playerName] == nil then
 		return
 	end
 
-	if code == keyboard.keys.up or 
+	if code == keyboard.keys.up or
 	   code == keyboard.keys.down or
 	   code == keyboard.keys.left or
 	   code == keyboard.keys.right or
@@ -34,11 +90,22 @@ end
 
 function handleRemoteInput(name, rcvAddr, sndAddr, port, distance, ...)
 	-- we don't have to check this at all right now, just pass it through
-	if #... == 4 then
+	local data = {...}
+	--
+	if data[1] == "getReceivers" then
+		loadReceivers()
+		modem.send(data[2], port, "sendReceivers", serialization.serialize(receivers))
+	end
+	if data[1] == "updateReceivers" then
+		updateReceivers(serialization.unserialize(data[3]))
+		modem.send(data[2], port, "ok")
+	end
+	if #data == 4 then
 		modem.send(DEST, PORT, serialization.serialize(...))
 	end
 end
 
+loadReceivers()
 
 event.listen("key_down", handleKeyPress)
 event.listen("modem_message", handleRemoteInput)
